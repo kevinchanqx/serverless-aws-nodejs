3cab3b3e1a588646507bf5c699f81ce34e01bfca
Kevin Chan <kevinchan950120@gmail.com>
2025-03-23 22:53:53 +0800

    add: api logger

---
+++ src/shared/utils/logger/api.logger.ts
@@ -0,0 +1,55 @@
+import _ from "lodash";
+import {
+  AxiosError,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+  isAxiosError,
+} from "axios";
+
+type InternalClientRequestConfig = InternalAxiosRequestConfig;
+type InternalClientResponseConfig = AxiosResponse;
+type InternalClientErrorConfig = AxiosError | Error;
+
+export const requestLog =
+  (name: string) => async (req: InternalClientRequestConfig) => {
+    console.log(`${name} Request:`, {
+      baseUrl: req.baseURL,
+      data: req.data,
+      headers: req.headers,
+      method: req.method,
+      params: req.params,
+      url: req.url,
+    });
+
+    return await Promise.resolve(req);
+  };
+
+export const responseLog =
+  (name: string) => async (res: InternalClientResponseConfig) => {
+    console.log(`${name} Response:`, {
+      status: res.status,
+      statusText: res.statusText,
+      data: res.data,
+    });
+    return await Promise.resolve(res);
+  };
+
+export const errorHandler =
+  (name: string) => async (err: InternalClientErrorConfig) => {
+    const error = {
+      name: err.name,
+      message: err.message,
+      stack: err.stack,
+    };
+
+    if (isAxiosError(err)) {
+      _.set(error, "code", err.code);
+      _.set(error, "status", err.status);
+      console.log(`${name} Error:`, error);
+
+      return await Promise.reject(err);
+    }
+
+    console.log(`${name} Error:`, error);
+    return await Promise.reject(err);
+  };
