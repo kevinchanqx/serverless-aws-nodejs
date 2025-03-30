9359b207304fee57a5359167926e2ebe1747190c
Kevin Chan <kevinchan950120@gmail.com>
2025-03-30 21:07:17 +0800

    chore: remove shared folder and change to utils

--- src/shared/utils/secret-manager/client.ts
+++
@@ -1,9 +0,0 @@
-import { SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
-
-let secretsManagerClient: SecretsManagerClient | undefined;
-
-export const getSecretsManagerClient = () => {
-  if (!secretsManagerClient) {
-    secretsManagerClient = new SecretsManagerClient({});
-  }
-};
