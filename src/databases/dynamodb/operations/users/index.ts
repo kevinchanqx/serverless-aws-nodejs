8c9e5b8aa421a22dada1caaefc35f9acf87afaf4
Kevin Chan <kevinchan950120@gmail.com>
2025-03-30 21:06:17 +0800

    add: operations functions for dynamodb

---
+++ src/databases/dynamodb/operations/users/index.ts
@@ -0,0 +1,35 @@
+import {
+  GetCommand,
+  GetCommandInput,
+  PutCommand,
+  PutCommandInput,
+} from "@aws-sdk/lib-dynamodb";
+import { DynamoDBTableNames } from "@databases/dynamodb/constants";
+import { getDynamoDBDocumentClient } from "@databases/dynamodb/client";
+import { User, UserPrimaryKey } from "@databases/dynamodb/types";
+
+const docClient = getDynamoDBDocumentClient();
+
+const usersTableName = DynamoDBTableNames.USERS;
+
+export const getUserFromDynamoDB = async (
+  input: Omit<GetCommandInput, "TableName"> & { Key: UserPrimaryKey },
+) => {
+  const getCommand = new GetCommand({
+    TableName: usersTableName,
+    ...input,
+  });
+
+  return (await docClient.send(getCommand)).Item;
+};
+
+export const createUserIntoDynamoDB = (
+  input: Omit<PutCommandInput, "TableName"> & { Item: User },
+) => {
+  const putCommand = new PutCommand({
+    TableName: usersTableName,
+    ...input,
+  });
+
+  return docClient.send(putCommand);
+};
