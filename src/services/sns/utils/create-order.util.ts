f61cb9e95581f0cca168d4a544bb5ef508f9be1c
Kevin Chan <kevinchan950120@gmail.com>
2025-04-05 17:16:50 +0800

    fix: wrong type for payload

--- src/services/sns/utils/create-order.util.ts
+++ src/services/sns/utils/create-order.util.ts
@@ -1,10 +1,11 @@
 import { publishMessage, SNSTopic } from "@shared/sns";
 import { CreateOrderSubscriberPayload } from "../types";
-import { CreateOrderBody } from "@services/sqs/types";
 
-export const publishCreateOrderMessage = async (body: CreateOrderBody) => {
+export const publishCreateOrderMessage = async (
+  payload: CreateOrderSubscriberPayload,
+) => {
   return publishMessage<CreateOrderSubscriberPayload>({
     topic: SNSTopic.CREATE_ORDER,
-    body,
+    body: payload,
   });
 };
